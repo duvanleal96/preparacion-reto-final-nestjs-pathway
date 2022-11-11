0686cf0b2ad47585aba626beae4e73f128bbad53
duvan leal <duvanleal65@gmail.com>
2022-11-11 17:38:20 -0500

    update

--- src/users/guards.auth.ts
+++ src/users/guards.auth.ts
@@ -7,13 +7,12 @@ export class GuardAuth implements CanActivate {
   canActivate(
     context: ExecutionContext,
   ): boolean | Promise<boolean> | Observable<boolean> {
-    const request = context.switchToHttp().getRequest().headers.autorization;
+    const request = context.switchToHttp().getRequest().headers.authorization;
     if (request) {
       const autorization = request.split(' ')[1];
       const key =
         '99e4c497d8b4c049ce41e71ff04055adc01714bccdf2f901e4c1d16ffabcef571398fa2b416ddc1037a98c4040c2f277c1bbb2dbc647ed65a2acaf657ac229ec';
-      console.log(request.body);
-      if (request != undefined && autorization == key) {
+      if (autorization != undefined && autorization == key) {
         return true;
       }
     }
--- src/users/http.middleware.ts
+++ src/users/http.middleware.ts
@@ -4,7 +4,6 @@ import { Request, Response, NextFunction } from 'express';
 @Injectable()
 export class HttpMiddleware implements NestMiddleware {
   use(req: Request, res: Response, next: NextFunction) {
-    console.log('Request Body...', req.body);
     if (req.params.uuid != undefined) {
       console.log(req.params);
     }
